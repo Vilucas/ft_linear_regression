2a32d69b02cbafc5d62a2413fa8373c2b5a22bd4
Victor Lucas <viclucas@e1z3r1p5.42.us.org>
2019-09-29 11:02:39 -0700

    :)

--- linear_regression.py
+++ linear_regression.py
@@ -6,27 +6,41 @@
 #    By: viclucas <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/09/22 12:56:05 by viclucas          #+#    #+#              #
-#    Updated: 2019/09/27 23:36:19 by viclucas         ###   ########.fr        #
+#    Updated: 2019/09/29 10:32:06 by viclucas         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import argparse
 from training import *
 
-
-global_m = 0
-global_b = 0
-
 if __name__ == "__main__":
-    result[]
+    result = []
+    data_dico = {}
+    line = ""
+
     try:
         print("Choose a mileage, we will give you a price approximation\n", "$> ", end="")
         s = int(input())
     except:
-       print("Wrong input :v")
+       print("Wrong input")
        exit()
-    f = open(".result.cvs")
-    line = f.readline()
-    line.split(",")
-    print("The estimated price for ur mileage input is :", end="")
-    print(int(line[0]) + (int(line[i]) * s))
+    try:
+        f = open(".result.cvs")
+        line = f.readline()
+    except:
+        s = 0 
+    if line:
+        line = line.split(",")
+    if s != 0:
+        try:
+            data_dico = average_compute(open("data.csv")) 
+        except:
+            print("give me my data file back :(")
+            exit()
+        s = s - min(data_dico['price_list'])
+        s /= (max(data_dico['price_list']) - min(data_dico['price_list']))
+        s = float(line[0]) + (float(line[1]) * float(s))
+        s *= (max(data_dico['price_list']) - min(data_dico['price_list']))
+        s = s + min(data_dico['price_list'])
+    print("The estimated price for your mileage input is : ", end="")
+    print(round(s))
--- training.py
+++ training.py
@@ -6,22 +6,11 @@
 #    By: viclucas <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/09/23 09:52:36 by viclucas          #+#    #+#              #
-#    Updated: 2019/09/27 23:44:32 by viclucas         ###   ########.fr        #
+#    Updated: 2019/09/29 10:32:06 by viclucas         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import  argparse
-import  numpy as np
-import  math
-
-def     data_denormalization(data):
-    for i in range(len(data['price_list'])):
-        data['price_list'][i] = data['price_list'][i] + min(data['price_list'])
-        data['price_list'][i] *= max(data['price_list']) + min(data['price_list']) 
-    for i in range(len(data['mileage_list'])):
-        data['mileage_list'][i] = data['mileage_list'][i] + min(data['mileage_list'])
-        data['mileage_list'][i] *= max(data['mileage_list']) + min(data['mileage_list']) 
-    return (data)
 
 def     data_normalization(data):
     for i in range(len(data['price_list'])):
@@ -36,7 +25,7 @@ def     gradient_descent(data_b):
     m = 0
     b = 0
     lr = 0.1
-    iteration = 1000
+    iteration = 215
     error = 0
     new_b = 0
     new_m = 0
@@ -50,11 +39,15 @@ def     gradient_descent(data_b):
             derror = 2 * (y_p - data_b['price_list'][i])
             derror_db += derror * 1
             derror_dm += derror * data_b['mileage_list'][i]
-        b = b - derror_db / len(data_b['price_list']) * lr
-        m = m - derror_dm / len(data_b['mileage_list']) * lr
+        i = 0;
+        b = b - (derror_db / len(data_b['price_list'])) * lr
+        m = m - (derror_dm / len(data_b['mileage_list'])) * lr
+        derror = 0
         derror_db = 0
         derror_dm = 0
-    return b, m
+    data_b['b'] = b
+    data_b['m'] = m
+    return data_b
 
 def     average_compute(averagef):
     price_list = []
@@ -80,14 +73,10 @@ def     average_compute(averagef):
     return data_b
 
 def     training():
-    global b_return
-    global m_return
-
     data_dico = {} #dico is a lingo word for dictionary in french
     parser = argparse.ArgumentParser(description='Example: python3 training data.csv')
     parser.add_argument('file', type=str, help=': the data file in the current directory')
     args = parser.parse_args()
-    solu = int(input())
     try:
         averagef = open(args.file)
         f = open(args.file)
@@ -95,14 +84,15 @@ def     training():
         print("open failled for '", args.file, "'")
         exit()
     data_dico = average_compute(averagef)
-    print(data_dico['price_list'])
-    #price_list, mileage_list, sd_price, sd_mileage
+    data_dico['o_price_list'] = list(data_dico['price_list'])
+    data_dico['o_mileage_list'] = list(data_dico['mileage_list'])
     data_dico = data_normalization(data_dico)
-    print(data_dico['price_list'])
-    b_return, m_return = gradient_descent(data_dico)
-    print(b_return + (m_return *solu))
-    exit()
-  #  data_dico = data_denormalization(data_dico)
+    data_dico = gradient_descent(data_dico)
+    f.close()
+    f = open(".result.cvs", "w")
+    s = str(data_dico['b']) + "," + str(data_dico['m'])
+    f.write(s)
+    return (data_dico)
 
 if  __name__ == "__main__":
     training()
